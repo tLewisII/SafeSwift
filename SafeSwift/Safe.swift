8701c1f1dc94a242d23b276f638d5b84a0a66c13
Terry Lewis <terrylewis@outlook.com>
2014-06-28 15:00:23 -0500

    Initial safe functions.

---
+++ SafeSwift/Safe.swift
@@ -0,0 +1,52 @@
+//
+//  Safe.swift
+//  SafeSwift
+//
+//  Created by Terry Lewis II on 6/28/14.
+//  Copyright (c) 2014 Blue Plover Productions. All rights reserved.
+//
+
+import Foundation
+
+extension Array {
+    func safeIndex(i:Int) -> T? {
+        if  i >= 0 && i < self.count {
+            return self[i]
+        } else {
+            return nil
+        }
+    }
+    
+    func safeHead() -> T? {
+        if !self.isEmpty {
+            return self[0]
+        } else {
+            return nil
+        }
+    }
+    
+    func safeTail() -> T[]? {
+        if self.count > 1 {
+            return Array(self[1..self.count])
+        } else {
+            return nil
+        }
+    }
+    
+    func safeRange(r:Array<Range<Int>>) -> T[]? {
+        if let range = r.safeHead() {
+            if range.startIndex >= 0 && range.endIndex < self.count {
+                return Array(self[range])
+            }
+        }
+        return nil
+    }
+}
+
+
+func safeUnwrap<T>(op:Optional<T>, defaultVal:T) -> T {
+    switch op {
+    case let .Some(a): return a
+    case .None: return defaultVal
+    }
+}
--- SafeSwiftTests/SafeSwiftTests.swift
+++ SafeSwiftTests/SafeSwiftTests.swift
@@ -7,7 +7,7 @@
 //
 
 import XCTest
-
+import SafeSwift
 class SafeSwiftTests: XCTestCase {
     
     override func setUp() {
@@ -20,16 +20,49 @@ class SafeSwiftTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+    func testSafeUnwrap() {
+        let val:String? = "Not nil"
+        XCTAssert(safeUnwrap(val, "") == "Not nil", "Should be \"Not nil\"")
+        
+        let nilVal:String? = nil
+        XCTAssert(safeUnwrap(nil, "nothing") == "nothing", "Should be \"nothing\"");
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    //    func testSafeIndex() {
+    //        let array = [1,2,3,4]
+    //
+    //        XCTAssert(array.safeIndex(1) == .Some(2), "Should be .Some(2)")
+    //        XCTAssert(array.safeIndex(4) == nil, "Should be nil")
+    //        XCTAssert(array.safeIndex(-456) == nil, "Should be nil")
+    //    }
     
+    //    func testSafeHead() {
+    //        let array = [1,2,3,4]
+    //        XCTAssert(array.safeHead() == .Some(1), "Should be .Some(1)")
+    //        XCTAssert(Array<Int>().safeHead() == nil, "Should be nil")
+    //    }
+    
+    //    func testSafeTail() {
+    //        let array = [1,2,3,4]
+    //        let tail = array.safeTail()
+    //        let shouldEQ:Int[]? = .Some([2,3,4])
+    //        if let unwrap = tail {
+    //            XCTAssert(unwrap == [2,3,4], "Should be equal")
+    //        } else {
+    //            XCTFail("Should never be reached")
+    //        }
+    //
+    //        XCTAssert(Array<Int>().safeTail() == nil, "Should be nil")
+    //    }
+    
+    //    func testSafeRange() {
+    //        let array = [1,2,3,4]
+    //        XCTAssert(array.safeRange([1...4]) == nil, "Should be nil")
+    //
+    //        if let unwrap = array.safeRange([1..4]) {
+    //            XCTAssert(unwrap == [2,3,4], "Should be [2,3,4]")
+    //        } else {
+    //            XCTFail("Should not be reached")
+    //        }
+    //}
 }
