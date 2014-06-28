f3b8869e1f2f67a84acd70c0b69f92d1df0d74e8
Terry Lewis <terrylewis@outlook.com>
2014-06-28 11:44:07 -0500

    Initial Commit

---
+++ SafeSwift/SafeSwift.h
@@ -0,0 +1,19 @@
+//
+//  SafeSwift.h
+//  SafeSwift
+//
+//  Created by Terry Lewis II on 6/28/14.
+//  Copyright (c) 2014 Blue Plover Productions. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SafeSwift.
+FOUNDATION_EXPORT double SafeSwiftVersionNumber;
+
+//! Project version string for SafeSwift.
+FOUNDATION_EXPORT const unsigned char SafeSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SafeSwift/PublicHeader.h>
+
+
---
+++ SafeSwiftTests/SafeSwiftTests.swift
@@ -0,0 +1,35 @@
+//
+//  SafeSwiftTests.swift
+//  SafeSwiftTests
+//
+//  Created by Terry Lewis II on 6/28/14.
+//  Copyright (c) 2014 Blue Plover Productions. All rights reserved.
+//
+
+import XCTest
+
+class SafeSwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
